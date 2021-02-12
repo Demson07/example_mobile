6367fb36b4b0bafa19e82b8db442c2dac594b2e3
dobidin <do@trinitydigital.ru>
2021-02-12 19:18:07 +0300

    in

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,19 +1,26 @@
 import io.appium.java_client.android.AndroidDriver;
+import org.junit.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class test {
+    AndroidDriver driver;
 
-    public static void setUp() throws MalformedURLException {
+    public void setUp() throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("deviceName", "pixel");
+        capabilities.setCapability("deviceName", "Pixel 4");
         capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("platformVersion", "9.0");
+        capabilities.setCapability("platformVersion", "11.0");
         capabilities.setCapability("udid", "emulator-5554");
-//        capabilities.setCapability();
-//        capabilities.setCapability();
-        AndroidDriver driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
+        capabilities.setCapability("appPackage", "com.materialstudies.reply");
+        capabilities.setCapability("appActivity", "com.materialstudies.reply.ui.MainActivity");
+        driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
+    }
+
+    @Test
+    public void testTest() {
+
     }
 }
